ab301624cea602ee4db8439e57f125f10ad2989c
sagarsaluja <sagarsaluja1998@gmail.com>
2022-03-11 16:58:28 +0530

    first changes

--- Learn.js
+++ Learn.js
@@ -6,7 +6,8 @@ let result = 1;
 const x = 2;
 var y = 3;
 console.log(result, x, y);
-//works because result , x , y is from this block's scope only
+//works because result , x is from this block's scope only
+//y is declared outside of a function , so it has block scope
 
 function A() {
   let b = 4;
@@ -16,12 +17,25 @@ function A() {
 }
 
 A();
+
+if (true) {
+  var m = 5;
+  let n = 10;
+  const w = 30;
+}
+console.log(m);
+//works as m is declared outside of a function , and has block scope
+
+//console.log(n , w);
+//does not work as n has scope of if only
+
+try {
+  console.log(n, w);
+} catch {
+  console.log("scope of n and w is of the if block only");
+}
 //prints 1 , as result is available throughout the block
 //prints 2 , 3 also
 
 //console.log(b , c , d);
 //this gives error , as b ,c ,d are declared in the function A , and is limited to the scope of A
-
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-}
