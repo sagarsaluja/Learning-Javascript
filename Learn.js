01b2fd31565c7fcb27dfdf07f80d5ce08a184b3f
sagarsaluja <sagarsaluja1998@gmail.com>
2022-03-11 17:22:55 +0530

    scope, re declaration and re assignment of let var and const

--- Learn.js
+++ Learn.js
@@ -1,19 +1,19 @@
 //Scope
 //let and const have block scope
-//var has function scope
+//var has function scope , if declared inside a function , global if declared outside a function
 //blocks can be  a function , if else block , for loop etc
-let result = 1;
+let r = 1;
 const x = 2;
 var y = 3;
-console.log(result, x, y);
-//works because result , x is from this block's scope only
-//y is declared outside of a function , so it has block scope
+console.log(r, x, y);
+//works because r and x have block scope
+//y is declared outside of a function , so it has global scope
 
 function A() {
   let b = 4;
   const c = 5;
   var d = 6;
-  console.log(result, x, y); //works
+  console.log(r, x, y); //works
 }
 
 A();
@@ -24,18 +24,41 @@ if (true) {
   const w = 30;
 }
 console.log(m);
-//works as m is declared outside of a function , and has block scope
-
-//console.log(n , w);
-//does not work as n has scope of if only
+//works as m is declared outside of a function , and has global scope
 
 try {
   console.log(n, w);
 } catch {
   console.log("scope of n and w is of the if block only");
 }
-//prints 1 , as result is available throughout the block
-//prints 2 , 3 also
 
-//console.log(b , c , d);
-//this gives error , as b ,c ,d are declared in the function A , and is limited to the scope of A
+//let can't be re declared , but var can be re-declared
+//so avoid using var
+//use let and const
+
+//var can be used before declaring it
+console.log(VarVariable);
+var VarVariable = 4;
+//this prints undefined
+console.log(VarVariable);
+//this prints 4
+
+//var can be re-declared
+var VarVariable = "Var-variable is now re-declared as string";
+console.log(VarVariable);
+
+//only difference between let and const is that const can't be re assigned to a different value
+let letVariable = 44;
+console.log(letVariable);
+letVariable = 55;
+console.log(letVariable);
+
+const constVariable = 50;
+console.log(constVariable);
+try {
+  constVariable = 40;
+} catch (error) {
+  console.log("can't re assign const");
+}
+
+//THINK ABOUT -> SCOPE, RE Declaration , Re Assignment when working with variables
