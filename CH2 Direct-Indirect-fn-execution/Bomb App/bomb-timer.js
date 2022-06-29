bd174a65b45c6c90522bfd0860016cf0e3d120a0
sagarsaluja <sagarsaluja1998@gmail.com>
2022-06-29 11:07:37 +0530

    bomb app complete

--- CH2 Direct-Indirect-fn-execution/Bomb App/bomb-timer.js
+++ CH2 Direct-Indirect-fn-execution/Bomb App/bomb-timer.js
@@ -5,20 +5,44 @@ const availableTime = 5 * 1000;
 
 let bombTimer;
 let remainingTime = availableTime;
-defuseButton.addEventListener("click", defuse);
+// defuseButton.addEventListener("click", defuse);
+init();
 //notice no parenthesis after defuse
 //basically we pass a pointer to the function here.
 //if the parenthesis are added , it'll directly execute the defuse button , when it reaches that line
 
-init();
+//but how do you pass parameters to defuse?
+//there are two approaches
+
+//first is to wrap difuse in an anonymous function
+//js puts this anonymous function into memory but doesn't execute it.
+// the pointer that has been passed to the add eventlistener is used by the browser
+//to call the anonymous function
+defuseButton.addEventListener("click", function () {
+  defuse("you won");
+});
+//you can also use the bind function
+// defuseButton.addEventListener("click", defuse.bind(null, "you won"));
+//by not using the anonymous function , you save some space.
+//by default every function in js is an object
+//and it has the bind method
+//bind allows you to pre configure the function on which you call it.
+//bind returns a pointer to a brand new function , based on the defuse function
+//this brand new function is "pre configured" to receive certain arguments in the following way:
+//the first argument to bind is the behaviour of the "this" keyword.
+//the next set of arguments are what are needed by the function which will be needed when its called.
+//the last argument is the "default event object" (the mouse event)
+//bind sends the last argument automatically ,you don't need to send it.
+//you can read it in the function by accepting event as a parameter
+
 //init calculates remaining time.
 //when time is <=0 , alerts the user that game is lost.
 function updateTimer() {
   timer.textContent = remainingTime / 1000;
 }
-function defuse() {
+function defuse(message) {
   clearInterval(bombTimer);
-  alert("defused");
+  alert(message);
 }
 function TimeHandler() {
   remainingTime = remainingTime - 1000;
@@ -35,4 +59,10 @@ function init() {
   //set interval calls the input function every 10 miliseconds , if time passed in is 10.
   //clearInterval stops the excution of setInterval
   bombTimer = setInterval(TimeHandler, 1000);
+
+  //setInterval can also take an anonymous function
+  //bombTimer = setInterval(function(){
+  //does something
+  //   }, 1000);
+  //notice we can add parenthesis after the anonymous function too , but haven't
 }
