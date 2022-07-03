ca0d7a70c1ca11359a3c6bc496cfb90a1ac21fdc
sagarsaluja <sagarsaluja1998@gmail.com>
2022-07-04 00:12:25 +0530

    coercion rules

--- CH5 Type Coercion/rules.js
+++ CH5 Type Coercion/rules.js
@@ -10,17 +10,21 @@
 //string + number is a string [number is converted to string]
 //string + string = string  [if both strings are numbers , they aren't coerced.]
 //number + boolean = number
+//bool + bool = number
+console.log("the + operator");
 console.log(3 + "hi");
 console.log(3 + 3);
 console.log("3" + "3"); //this is different for the '-' operator!!!!
 console.log(3 + "3");
 console.log(3 + true, 3 + false); //number
 console.log("3" + true); //string.
+console.log("the + operator over");
 
 // == , != , && ,|| , < , > operators
-console.log("comparison operators");
-//if a number and a string are compared , both are converted to a number
-//a string converted to a number is NaN , which gives output of false on comparing to a number.
+console.log("comparison operators : < , > , ==");
+console.log("logical operators: && , ||  ");
+//if a number and a string are compared , string is converted to a number
+//a string converted to a number is NaN , which gives output of false on comparing to anything
 //if both are strings , hierarchy is followed.
 //comparison of strings follows this hierarchy: 01234...9abcdef...zABCD...Z
 console.log(5 > 3); //numbers are compared
@@ -32,7 +36,18 @@ console.log("A" < "a"); //follows hierarchy
 console.log("hi" > "hare"); //follows hierarchy
 console.log("hi" < "hare"); //follows hierarchy
 console.log("2" > "12"); //follows hierarchy
+console.log("5" > 2); //
+console.log("5" > true); //5 is converted to a number , true is converted to 1
+console.log("hi" > true); //compared NaN , gives false.
 console.log("over");
+
+console.log("the == operator");
+//rule : convert to number if two different datatypes are compared
+console.log("true" == true); //compares NaN to true => outputs false.
+console.log(true == true);
+console.log(0 == true);
+console.log("" == false); //output is true , because an empty string converts to 0
+
 //more on boolean later.
 
 //for - , * , / , %
